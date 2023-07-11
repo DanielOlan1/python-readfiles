f89b956764fda282c060ef665f580aabb7ac68d4
Esteban Carrera <127622233+EstebanCarr@users.noreply.github.com>
2023-07-11 17:41:00 -0600

    aaaa

--- suma.py
+++ suma.py
@@ -10,16 +10,30 @@ class Aplicacion(tk.Tk):
         self.tabla_frame = None
         self.tabla = None
         self.formulario_frame = None
+        self.formulario_texto_operador = None
+        self.formulario_texto_unidad = None
         self.formulario_texto = None
 
         boton_abrir = tk.Button(self, text="Abrir archivo de Excel", command=self.abrir_archivo_excel)
-        boton_abrir.pack()
+        boton_abrir.pack(pady=10)
+        self.geometry("400x300")
 
     def mostrar_datos(self, event):
         fila_seleccionada = self.tabla.focus()
         datos_fila = self.tabla.item(fila_seleccionada)['values']
+
+        operador = datos_fila[0]  # Obtén el valor de la primera columna (operador)
+        unidad = datos_fila[1]  # Obtén el valor de la segunda columna (unidad)
+        texto_fila = '\n'.join(str(dato) for dato in datos_fila[2:])  # Resto de las columnas
+
+        self.formulario_texto_operador.delete(1.0, tk.END)  # Limpia el cuadro de texto del operador
+        self.formulario_texto_operador.insert(tk.END, operador)  # Inserta el valor del operador
+
+        self.formulario_texto_unidad.delete(1.0, tk.END)  # Limpia el cuadro de texto de la unidad
+        self.formulario_texto_unidad.insert(tk.END, unidad)  # Inserta el valor de la unidad
+
         self.formulario_texto.delete(1.0, tk.END)
-        self.formulario_texto.insert(tk.END, str(datos_fila))
+        self.formulario_texto.insert(tk.END, texto_fila)
 
     def abrir_archivo_excel(self):
         archivo = filedialog.askopenfilename(filetypes=[("Archivos de Excel", "*.xlsx;*.xls")])
@@ -83,6 +97,18 @@ class Aplicacion(tk.Tk):
             # Crea el formulario para mostrar los datos de la fila seleccionada
             self.formulario_frame = tk.Frame(self)
             self.formulario_frame.pack(fill=tk.BOTH, expand=True)
+            
+            formulario_label_operador = tk.Label(self.formulario_frame, text="Operador:")
+            formulario_label_operador.pack(side=tk.TOP)
+            
+            self.formulario_texto_operador = tk.Text(self.formulario_frame, height=1, width=100)
+            self.formulario_texto_operador.pack(side=tk.TOP)
+            
+            formulario_label_unidad = tk.Label(self.formulario_frame, text="Unidad:")
+            formulario_label_unidad.pack(side=tk.TOP)
+            
+            self.formulario_texto_unidad = tk.Text(self.formulario_frame, height=1, width=100)
+            self.formulario_texto_unidad.pack(side=tk.TOP)
             
             formulario_label = tk.Label(self.formulario_frame, text="Datos de la fila seleccionada:")
             formulario_label.pack(side=tk.TOP)

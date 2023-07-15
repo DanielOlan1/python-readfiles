14d2e7a5f9659b3166ba572fa0d19aa126ba8fc4
Esteban Carrera <127622233+EstebanCarr@users.noreply.github.com>
2023-07-15 11:02:50 -0600

    Faltantes y sobrantes

--- carta_porte_faltantes_sobrantes.py
+++ carta_porte_faltantes_sobrantes.py
@@ -4,26 +4,48 @@ from tkinter import messagebox
 import pandas as pd
 
 def mostrar_cartas_faltantes_sobrantes(df):
-    cartas_faltantes_sobrantes = [
-        {
-            "operador": fila["operador"],
-            "unidad": fila["unidad"],
-            "letraPR": fila["letraPR"],
-            "origenMunicipio": fila["origenMunicipio"],
-            "destinoMunicipio": fila["destinoMunicipio"],
-            "cliente": fila["cliente"],
-            "producto": fila["producto"],
-            "cartaPorte": fila["cartaPorte"],
-            "cancelada": "Yes" if fila["cancelada"] else "No",
-            "faltante": fila["faltante"]
-        }
-        for _, fila in df.iterrows()
-        if fila["fechaDescarga"] != 0
-    ]
+    filas_verde = []
+    filas_rojo = []
+
+    def toggle_seleccion():
+        seleccion = casilla_var.get()
+        casilla_var.set(not seleccion)
+
+    for _, fila in df.iterrows():
+        if fila["fechaDescarga"] != 0 and fila["faltante"] != fila["litrosCargados"]:
+            carta_porte_data = {
+                "operador": fila["operador"],
+                "unidad": fila["unidad"],
+                "letraPR": fila["letraPR"],
+                "origenMunicipio": fila["origenMunicipio"],
+                "destinoMunicipio": fila["destinoMunicipio"],
+                "cliente": fila["cliente"],
+                "producto": fila["producto"],
+                "cartaPorte": fila["cartaPorte"],
+                "faltante": fila["faltante"]
+            }
+
+            if carta_porte_data["faltante"] <= 0:
+                filas_verde.append(carta_porte_data)
+            else:
+                filas_rojo.append(carta_porte_data)
+
+    cartas_faltantes_sobrantes = filas_verde + filas_rojo
 
     if cartas_faltantes_sobrantes:
         ventana_cartas_faltantes_sobrantes = tk.Toplevel()
-        ventana_cartas_faltantes_sobrantes.title("Cartas Porte Faltantes y Sobrantes")
+        ventana_cartas_faltantes_sobrantes.title("Cartas Porte Faltantes/Sobrantes")
+
+        casilla_var = tk.BooleanVar()
+        casilla_checkbutton = tk.Checkbutton(
+            ventana_cartas_faltantes_sobrantes,
+            text="A Sisa",
+            variable=casilla_var,
+            onvalue=True,
+            offvalue=False,
+            command=toggle_seleccion
+        )
+        casilla_checkbutton.pack()
 
         tabla_frame = tk.Frame(ventana_cartas_faltantes_sobrantes)
         tabla_frame.pack(fill=tk.BOTH, expand=True)
@@ -43,7 +65,6 @@ def mostrar_cartas_faltantes_sobrantes(df):
             "cliente",
             "producto",
             "cartaPorte",
-            "cancelada",
             "faltante"
         ]
         tabla["columns"] = columnas
@@ -53,23 +74,19 @@ def mostrar_cartas_faltantes_sobrantes(df):
 
         for carta_porte_data in cartas_faltantes_sobrantes:
             fila_values = [
-                str(carta_porte_data[col]) if not pd.isna(carta_porte_data[col]) else ""
-                for col in columnas[:-2]  # Exclude "cancelada" and "faltante" columns
+                carta_porte_data[col] if not pd.isna(carta_porte_data[col]) else ""
+                for col in columnas
             ]
-            fila_values.append(carta_porte_data["cancelada"])
-            fila_values.append(carta_porte_data["faltante"])
-
-            if carta_porte_data["faltante"] < 0:
-                tabla.insert("", tk.END, values=fila_values, tags=("sobrante",))
-            elif carta_porte_data["faltante"] > 0:
-                tabla.insert("", tk.END, values=fila_values, tags=("faltante",))
+            faltante = carta_porte_data["faltante"]
+            if faltante <= 0:
+                tabla.insert("", tk.END, values=fila_values, tags=("verde",))
             else:
-                tabla.insert("", tk.END, values=fila_values)
+                tabla.insert("", tk.END, values=fila_values, tags=("rojo",))
 
-        tabla.tag_configure("sobrante", foreground="green")
-        tabla.tag_configure("faltante", foreground="red")
+        tabla.tag_configure("verde", foreground="green")
+        tabla.tag_configure("rojo", foreground="red")
 
         tabla.pack(fill=tk.BOTH, expand=True)
 
     else:
-        messagebox.showinfo("Cartas Porte Faltantes y Sobrantes", "No hay Cartas Porte Faltantes y Sobrantes para mostrar.")
+        messagebox.showinfo("Cartas Porte Faltantes/Sobrantes", "No hay Cartas Porte Faltantes/Sobrantes para mostrar.")

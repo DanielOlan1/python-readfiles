8574361f6226d9383df576a3bbe3870950dd6cf8
Esteban Carrera <127622233+EstebanCarr@users.noreply.github.com>
2023-07-17 11:50:40 -0600

    CODIGO

--- carta_porte_faltantes_sobrantes.py
+++ carta_porte_faltantes_sobrantes.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import filedialog
 import pandas as pd
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
 
 def mostrar_cartas_faltantes_sobrantes(df):
     filas_verde = []
@@ -10,6 +14,60 @@ def mostrar_cartas_faltantes_sobrantes(df):
     def toggle_seleccion():
         seleccion = casilla_var.get()
         casilla_var.set(not seleccion)
+
+    def borrar_filas():
+        seleccionadas = tabla.selection()
+        if seleccionadas:
+            confirmacion = messagebox.askyesno(
+                "Confirmar eliminación",
+                "¿Estás seguro de que deseas eliminar las filas seleccionadas?"
+            )
+            if confirmacion:
+                for item in seleccionadas:
+                    tabla.delete(item)
+        else:
+            messagebox.showinfo("Sin selección", "No se ha seleccionado ninguna fila.")
+
+    def exportar_pdf():
+        folder_path = filedialog.askdirectory()
+        if folder_path:
+            filename = filedialog.asksaveasfilename(
+                defaultextension=".pdf",
+                filetypes=[("PDF Files", "*.pdf")],
+                initialdir=folder_path
+            )
+            if filename:
+                doc = SimpleDocTemplate(filename, pagesize=letter)
+
+                data = [["Operador", "Unidad", "Letra PR", "Origen", "Destino", "Cliente", "Producto", "Carta Porte", "Faltante"]]
+                for item in tabla.get_children():
+                    row_data = []
+                    for value in tabla.item(item)['values']:
+                        if value:
+                            row_data.append(value)
+                        else:
+                            row_data.append("")
+                    data.append(row_data)
+
+                table = Table(data)
+                style = TableStyle([
+                    ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                    ('FONTSIZE', (0, 0), (-1, 0), 12),
+                    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                    ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                    ('GRID', (0, 0), (-1, -1), 1, colors.black),
+                ])
+                table.setStyle(style)
+
+                elements = [table]
+                doc.build(elements)
+
+                messagebox.showinfo("Exportación exitosa", f"Los datos se han exportado correctamente a {filename}")
+        else:
+            messagebox.showinfo("Exportación cancelada", "No se ha seleccionado ninguna ubicación.")
 
     for _, fila in df.iterrows():
         if fila["fechaDescarga"] != 0 and fila["faltante"] != fila["litrosCargados"]:
@@ -87,6 +145,20 @@ def mostrar_cartas_faltantes_sobrantes(df):
         tabla.tag_configure("rojo", foreground="red")
 
         tabla.pack(fill=tk.BOTH, expand=True)
+
+        boton_borrar = ttk.Button(
+            ventana_cartas_faltantes_sobrantes,
+            text="Borrar Filas Seleccionadas",
+            command=borrar_filas
+        )
+        boton_borrar.pack(side=tk.BOTTOM, pady=10)
+
+        boton_exportar = ttk.Button(
+            ventana_cartas_faltantes_sobrantes,
+            text="Exportar a PDF",
+            command=exportar_pdf
+        )
+        boton_exportar.pack(side=tk.BOTTOM, pady=10)
 
     else:
         messagebox.showinfo("Cartas Porte Faltantes/Sobrantes", "No hay Cartas Porte Faltantes/Sobrantes para mostrar.")
